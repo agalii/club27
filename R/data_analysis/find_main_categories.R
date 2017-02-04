b0d45276a63759c05893d481a9618a5d8a69b0bb
agalii <git@lisa-angermann.de>
2017-02-04 20:59:11 +0100

    plot and analysis for stacked: moved all analysis to analysis script, cosmetics and new functions for simplification

--- R/data_analysis/find_main_categories.R
+++ R/data_analysis/find_main_categories.R
@@ -1,5 +1,5 @@
 ##################################################################################################
-# properties to clusters
+# properties to clusters/categories
 # use only the first mentioned valid property to identify the main cluster 
 
 fn <- 'data/output/intermediate_backups/simplified_properties_wide.csv'
@@ -13,12 +13,10 @@ sport <- unique(as.vector(sport[,1]))
 music  <- read.csv(file = 'data/additional/music_clean.csv',  stringsAsFactors = F)
 music  <- unique(as.vector(music[,1]))
 
-# Jurist, arzt, psychologe/psychoanalytike/psychater, Architekt, Ingenieur, Richter, Diplomat, adliger
 cat <- c('musician', 'singer', 'actor', 'athlete', 'politician', 'showbiz', 'author', 'scientist', 'cleric', 'military', 'resistance', 'journalist', 'artist')
 att <- list(c('musiker$', 'komponist$', 'dirigent$', 'organist$', 'schlagzeuger$', 'virtuos.?$', music), 
             c('sänger$'), 
             c('schauspieler$', 'theaterspieler$'), 
-            # c('tänzer'),
             c('sportler$', 'läufer$', sport), 
             c('politiker$', 'gouverneur$', 'goveneur$', 'präsident$', 'mdl$', 'mdb$', 'minister$', 'bürgermeister$'), 
             c('moderator$', 'entertainer$', 'regisseur$', 'kamera(mann$|frau$)', 'kabrettist$'), 
@@ -26,32 +24,12 @@ att <- list(c('musiker$', 'komponist$', 'dirigent$', 'organist$', 'schlagzeuger$
             c('wissenschaftler$','mathematiker$', 'olog(e$|$)','forscher$', 'geodät$', 
               'kartograph$', 'historiker$', 'ökonom$', 'geograph$', 'linguist$', 'germanist$', 
               'botaniker$', 'physiker$', 'chemiker$', 'astronom$', 'anatom$', 'mediziner$', 'philosoph$'), 
-            c('geistliche(r$|$)', 'bischof$', 'pfarrer$', 'rabbi$', 'pastor$', 'kardinal$', 'prediger$', 'missionar$'), 
+            c('geistliche(r$|$)', 'bischof$', 'pfarrer$', 'rabbi$', 'imam', 'pastor$', 'kardinal$', 'prediger$', 'missionar$'), 
             c('offizier$', 'general$', 'leutnant$', 'mayor$', 'kommandant$',  
               'admiral$', 'diktator$', 'marshall$', 'kriegsherr$', 'brigadeführer$', 'soldat$'), 
             c('freiheitskämpfer$', 'befreiungskrieg$', 'unabhängigkeitskrieg$', 'revolutionär$', '^rose$', 'regimekritiker$', 'aktivist$', 'aufstand'), 
             c('journalist$', 'berichterstatter$'), 
             c('maler$', 'künstler$', 'zeichner$', 'grafiker$', 'graphiker$', 'skulpturist$', 'bildhauer$', 'karikaturist$')) 
-
-# c('holocaust', '$kriegsjustiz') + 'opfer')
-
-
-#####################################################################################################
-# wiki_long <- wiki_data
-# wiki_data <- wiki_data[1:100, ]
-
-# get_cat_per_p <- function(data, p, cat, att) {
-#   for (i in 1:length(cat)) {
-#     for (a in att[[i]]) {
-#       i_a <- grep(a, data[ , p])  
-#       i_a1 <- i_a[which(is.na(data$main[i_a]))]
-#       i_a2 <- i_a[which(!is.na(data$main[i_a]))]
-#       data$main[i_a1] <- cat[i]
-#       data$main2[i_a2] <- cat[i]
-#     }
-#   }
-#   return(data)
-# }
 
 
 # apply columnwise, loop through properties
@@ -61,17 +39,15 @@ wd <- wiki_data[, c(1, ip)]
 done <- NULL
 
 for (c in 2:ncol(wd)) {
-  wd$main <- NA
+  wd$main  <- NA
   wd$main2 <- NA # main2 only needed to check for ambiguity
-  
-  # wd <- get_cat_per_p(wd, c, cat, att)
   
   for (i in 1:length(cat)) {
     for (a in att[[i]]) {
       i_a  <- grep(a, wd[ , c])  
       i_a1 <- i_a[which(is.na(wd$main[i_a]))]
       i_a2 <- i_a[which(!is.na(wd$main[i_a]))]
-      wd$main[i_a1] <- cat[i]
+      wd$main[i_a1]  <- cat[i]
       wd$main2[i_a2] <- cat[i]
     }
   }
@@ -82,13 +58,48 @@ for (c in 2:ncol(wd)) {
     print(paste('Second step error within property', c))
     break
   }
-  wd       <- wd[which(is.na(wd$main) & is.na(wd$main2)), ]
+  wd   <- wd[which(is.na(wd$main) & is.na(wd$main2)), ]
 }
 
 # remaining entries are not covered by defined categories and attributes and summarized as 'others' 
-wd$main <- 'other'
-done     <- rbind(wd, done)
+wd$main   <- 'other'
+done      <- rbind(wd, done)
 
 wiki_main <- merge(wiki_data[, 1:8], done[, c('title', 'main')], by = 'title', all = T)
 
 write.csv(wiki_main, file = 'data/output/main_categories.csv', row.names = F)
+
+
+# prepare data as needed for stacked_comparison plot ############################
+# fn <- 'data/output/main_categories.csv'
+# wiki_main <- read.csv(file = fn, stringsAsFactors = F)
+
+fn       <- 'data/output/data_to_plot/aux_data_for_plots.csv'
+aux_data <- read.csv(file = fn, stringsAsFactors = F)
+
+club27 <- subset(wiki_main, age == 27)
+others <- subset(wiki_main, age != 27)
+
+dist_table <- function(data, aux) {
+  cat         <- as.data.frame(table(data), responseName = 'total')
+  cat$percent <- 100 * cat$total / sum(cat$total)
+  cat         <- merge(cat, aux, by.x = 'data', by.y = 'cat', all = T)
+  return(cat)
+}
+
+cat_c27    <- dist_table(club27$main, aux_data)
+cat_all    <- dist_table(wiki_main$main, aux_data)
+cat_others <- dist_table(others$main, aux_data)
+
+# order both data sets according percentage of in-/decrease 
+cat_c27$gain <- (cat_c27$percent - cat_all$percent) / cat_all$percent
+i_order      <- order(-cat_c27$gain)
+cat_c27      <- cat_c27[i_order,] 
+cat_all      <- cat_all[i_order,]
+cat_others   <- cat_others[i_order,]
+
+
+write.csv(cat_c27, file = 'data/output/data_to_plot/group_distribution_c27.csv', row.names = F)
+write.csv(cat_all, file = 'data/output/data_to_plot/group_distribution_all.csv', row.names = F)
+write.csv(cat_others, file = 'data/output/data_to_plot/group_distribution_all_but_c27.csv', row.names = F)
+
--- R/plot/stacked_comparison.R
+++ R/plot/stacked_comparison.R
@@ -3,63 +3,84 @@ font.add('default', 'fonts/urw-gothic-l-book.ttf')
 font.add('ubuntu', 'fonts/Ubuntu-L.ttf') # for special characters only
 
 
-fn        <- 'data/output/data_to_plot/aux_data_for_plots.csv'
+# load required data
+fn       <- 'data/output/data_to_plot/aux_data_for_plots.csv'
 aux_data <- read.csv(file = fn, stringsAsFactors = F)
 
-fn <- 'data/output/main_categories.csv'
-wiki_main <- read.csv(file = fn, stringsAsFactors = F)
-# str(wiki_main)
+fn_c27  <- 'data/output/data_to_plot/group_distribution_c27.csv'
+cat_c27 <- read.csv(file = fn_c27, stringsAsFactors = F)
 
-club27 <- subset(wiki_main, age == 27)
-others <- subset(wiki_main, age != 27)
+# choose whether to include club 27 in reference sample or not
+fn_ref  <- 'data/output/data_to_plot/group_distribution_all.csv'
+# fn_ref  <- 'data/output/data_to_plot/group_distribution_all_but_c27.csv'
+cat_all <- read.csv(file = fn_ref, stringsAsFactors = F)
 
-cat_c27 <- as.data.frame(table(club27$main), responseName = 'total')
-# cat_all <- as.data.frame(table(wiki_main$main), responseName = 'total')
-cat_all <- as.data.frame(table(others$main), responseName = 'total')
 
-cat_all$percent <- 100 * cat_all$total / sum(cat_all$total)
 
-cat_c27$percent <- 100 * cat_c27$total / sum(cat_c27$total)
-cat_all <- merge(cat_all, aux_data, by.x = 'Var1', by.y = 'cat', all = T)
-cat_c27 <- merge(cat_c27, aux_data, by.x = 'Var1', by.y = 'cat', all = T)
+# define parameters and functions needed during plotting ################
+# index to split figure in gaining and loosing groups
+i_center <- max(which(cat_c27$gain >= 0))
 
-# order both data sets according percentage of in-/decrease
-cat_c27$gain <- (cat_c27$percent - cat_all$percent) / cat_all$percent
-i_order <- order(-cat_c27$gain)
-cat_all <- cat_all[i_order,]
-cat_c27 <- cat_c27[i_order,] 
-i_centre <- max(which(cat_c27$gain >= 0))
-
+# handmade arrow...
 arrow <- data.frame(x = c(0, -0.25, -0.14, -0.4, 0, 0.4, 0.14, 0.21, 0), 
                     y = c(0, -0.5, 26.5, 26, 39, 25, 26, +0.5, 0))
+# standard grey...
+sg    <- rgb(0.8,0.8,0.8)
 
-svg(filename = 'figure_drafts/stacked_comparison_sinus_up_down.svg', width = 10, height = 7, pointsize = 12, onefile = T)
+# color scale transformation function
+cstf   <- function(data) {(3-sqrt(abs(data)))/2.9} 
+
+# plot sinus wave polygon to link left and right rectangles
+sinus_polygon <- function(c_off, a_off, c_per, a_per, grey_value = 0.8) {
+  # left and right border for link with sinus polygons
+  l <- 2.2
+  r <- 7.8
+  xp <- seq(l, r, length.out = 20)
+  
+  amp_b  <- (c_off - a_off) / 2
+  yoff_b <- a_off + amp_b
+  lambda <- (8.2 - 1.8) * 0.25  # wave length
+  xoff   <- 2.4 + lambda / 2
+  bot    <- amp_b*sin(xoff+xp/lambda) + yoff_b
+  
+  amp_t  <- (c_per + c_off - (a_per + a_off)) / 2
+  yoff_t <- a_per + a_off + amp_t
+  top    <- rev(amp_t*sin(xoff+xp/lambda) + yoff_t)
+  
+  lc     <- rgb(grey_value, grey_value, grey_value, maxColorValue = 1)
+  
+  polygon(c(xp, rev(xp)), c(bot, top), col = lc, border = lc, xpd = T)
+}
+
+
+# ######################################################### plot
+svg_file <- 'figure_drafts/stacked_comparison_up_down.svg'
+svg(filename = svg_file, width = 10, height = 7, pointsize = 12, onefile = T)
 par(mar = c(0, 0, 0, 0), family = 'default') 
 showtext.begin() # has to be removed if font in svg needs to changed subsequntly
 
 cat_all$off <- NA
 cat_c27$off <- NA
 
-plot(1, 1, type = 'n', xlim = c(-1.5, 13.5), ylim = c(-100, 140), 
-     xlab = '', ylab = '', frame.plot = F) #, axes = F)
+plot(1, 1, type = 'n', xlim = c(-1.5, 13.5), ylim = c(-95, 145), 
+     xlab = '', ylab = '', frame.plot = F, axes = F)
 
+# auxiliar drawings
 points(c(-1.23, 13), c(0, 0), type = 'l', lwd = 3, col = rgb(0.3,0.3,0.3))
-polygon(0.8*arrow$x-1 , 5+arrow$y, col = rgb(0.8,0.8,0.8), border = rgb(0.8,0.8,0.8), xpd = T)
+polygon(0.8*arrow$x-1 , 5+arrow$y, col = sg, border = sg, xpd = T)
 text(-0.5, 5, 'Höherer Anteil am Club 27', srt = 90, pos = 4)
-polygon(-0.8*arrow$x-1 , -5-arrow$y, col = rgb(0.8,0.8,0.8), border = rgb(0.8,0.8,0.8), xpd = T)
+polygon(-0.8*arrow$x-1 , -5-arrow$y, col = sg, border = sg, xpd = T)
 text(-0.5, -55.6, 'Geringerer Anteil', srt = 90, pos = 4)
 
-# left and right border for link with sinus polygons
-l <- 2.2
-r <- 7.8
-xp <- seq(l, r, length.out = 20)
-
+# draw rectangles and linking polygons
 # offsets between rectangles and first offset
 inter <- 5 
 all_off <- inter
 c27_off <- inter
 
-for (i in i_centre:1) {
+######################################################################
+# plot all groups with INcreased percentage in club 27
+for (i in i_center:1) {
   color <- cat_all$color[i]
   cat_all$off[i] <- all_off
   rect(0, all_off, 2, cat_all$percent[i] + all_off, col = color, border = color, xpd = T)
@@ -67,32 +88,13 @@ for (i in i_centre:1) {
   cat_c27$off[i] <- c27_off
   rect(8, c27_off, 10, cat_c27$percent[i] + c27_off, col = color, border = color, xpd = T)
   
+  # print label next to rectangle
   ytext <- c27_off + cat_c27$percent[i] / 2
   text(10.3, ytext, labels =  cat_c27$legend[i], pos = 4)
   points(c(10, 10.35), c(ytext, ytext), type = 'l', col = color)
   
-  # plot sinu wave polygon link
-  amp_b  <- (c27_off - all_off) / 2
-  yoff_b <- all_off + amp_b
-  lambda <- (8.2 - 1.8) * 0.5 * 0.5                        # wave length
-  xoff <- 2.4 + lambda / 2
-  
-  bot <- amp_b*sin(xoff+xp/lambda)+yoff_b
-  
-  amp_t  <- (cat_c27$percent[i] + c27_off - (cat_all$percent[i] + all_off)) / 2
-  yoff_t <- cat_all$percent[i] + all_off + amp_t
-  
-  top <- rev(amp_t*sin(xoff+xp/lambda) + yoff_t)
-  
-  # zero <- 7.8
-  # maxc <- 8
-  # lc <- rgb(zero-cat_c27$gain[i], zero-cat_c27$gain[i], zero-cat_c27$gain[i], maxColorValue = maxc)
-  # zero <- 2.8
-  # maxc <- 2.8
-  zero <- 3
-  maxc <- 2.9
-  lc <- rgb(zero-sqrt(cat_c27$gain[i]), zero-sqrt(cat_c27$gain[i]), zero-sqrt(cat_c27$gain[i]), maxColorValue = maxc)
-  polygon(c(xp, rev(xp)), c(bot, top), col = lc, border = lc, xpd = T)
+  # plot sinus wave polygon link
+  sinus_polygon(c27_off, all_off, cat_c27$percent[i], cat_all$percent[i], cstf(cat_c27$gain[i]))
   
   # for the next step
   all_off <- all_off + cat_all$percent[i] + inter
@@ -103,14 +105,17 @@ text(9, cat_c27$percent[1] + cat_c27$off[1] + 3, labels = 'Club 27', pos = 3, ce
 text(1, cat_all$percent[1] + cat_all$off[1] + 3, labels = 'Gesamt', pos = 3, cex = 1.5)
 
 ######################################################################
-# Up and down 
-# start at the bottom ---> bottom needs to be calculated first
-all_off <- -sum(inter + cat_all$percent[(i_centre+1):nrow(cat_all)])
+# plot all groups with DEcreased percentage in club 27
+# start at the bottom --> bottom offset needs to be calculated first
+all_off <- -sum(inter + cat_all$percent[(i_center+1):nrow(cat_all)])
 c27_off <- all_off
+
+# compensate for too narrow spaces to properly print labels
+# theshold indicates minimum space needed
 ytext_o <- all_off-12
 threshold <- 12
 
-for (i in nrow(cat_c27):(i_centre+1)) {
+for (i in nrow(cat_c27):(i_center+1)) {
   color <- cat_all$color[i]
   cat_all$off[i] <- all_off
   rect(0, all_off, 2, cat_all$percent[i] + all_off, col = color, border = color, xpd = T)
@@ -118,7 +123,7 @@ for (i in nrow(cat_c27):(i_centre+1)) {
   cat_c27$off[i] <- c27_off
   rect(8, c27_off, 10, cat_c27$percent[i] + c27_off, col = color, border = color, xpd = T)
   
-  # plot label
+  # plot labels, either in center of rectangle or one threshold above last one
   ytext <- c27_off + cat_c27$percent[i] / 2
   if ((ytext - ytext_o) > threshold) {
     text(10.3, ytext, labels =  cat_c27$legend[i], pos = 4)
@@ -130,46 +135,38 @@ for (i in nrow(cat_c27):(i_centre+1)) {
     points(c(10, 10.35), c(cat_c27$percent[i] + c27_off, ytext_o), type = 'l', col = color) #rgb(0.3,0.3,0.3)
   }
   
-  # plot sinu wave polygon link
-  amp_b  <- (c27_off - all_off) / 2
-  yoff_b <- all_off + amp_b
-  lambda <- (8.2 - 1.8) * 0.5 * 0.5                        # wave length
-  xoff <- 2.4 + lambda / 2
-  
-  bot <- amp_b*sin(xoff+xp/lambda)+yoff_b
-  
-  amp_t  <- (cat_c27$percent[i] + c27_off - (cat_all$percent[i] + all_off)) / 2
-  yoff_t <- cat_all$percent[i] + all_off + amp_t
-  
-  top <- rev(amp_t*sin(xoff+xp/lambda) + yoff_t)
-  
-  # lc <- rgb(zero+cat_c27$gain[i], zero+cat_c27$gain[i], zero+cat_c27$gain[i], maxColorValue = maxc)
-  lc <- rgb(zero-sqrt(abs(cat_c27$gain[i])), zero-sqrt(abs(cat_c27$gain[i])), zero-sqrt(abs(cat_c27$gain[i])), maxColorValue = maxc)
-  polygon(c(xp, rev(xp)), c(bot, top), col = lc, border = lc, xpd = T)
+  # plot sinu wave polygon link between rectangles
+  sinus_polygon(c27_off, all_off, cat_c27$percent[i], cat_all$percent[i], cstf(cat_c27$gain[i]))
   
   # for the next step
   all_off <- all_off + cat_all$percent[i] + inter
   c27_off <- c27_off + cat_c27$percent[i] + inter
 }
 
-# plot legend
-# leg_lab <- seq(80, 780, by = 100)
+# plot legend for grey scale manually #####################################
 leg_lab <- c(3, 30, 60, 90, 300, 780)
 
-# lc <- rgb(zero-sqrt(cat_c27$gain[i]), zero-sqrt(cat_c27$gain[i]), zero-sqrt(cat_c27$gain[i]), maxColorValue = maxc)
-
-leg_col <- rgb(zero-sqrt(leg_lab/100), zero-sqrt(leg_lab/100), zero-sqrt(leg_lab/100), maxColorValue = maxc)
+leg_col <- rgb(cstf(leg_lab/100), cstf(leg_lab/100), cstf(leg_lab/100))
 x1 <- 0
 for (i in 1:length(leg_lab)) {
   rect(x1, 135, x1+0.35, 130, col = leg_col[i], border = leg_col[i])
   text(x1+0.175, 130, labels = leg_lab[i], pos = 1, cex = 0.8)
   x1 <- x1 + 0.65
 }
+
 text(x1-0.13, 130, labels = '%', pos = 1, cex = 0.8)
 text(-0.2, 141, labels = 'Mehr oder weniger als in der Gesamtmenge', pos = 4)
 
 showtext.end()
 dev.off()
+
+
+
+
+
+
+
+
 
 ######################################################################
 # All to the same direction
@@ -179,7 +176,7 @@ dev.off()
 # ytext_o <- -2
 # threshold <- 9
 # 
-# for (i in (i_centre+1):nrow(cat_c27)) {
+# for (i in (i_center+1):nrow(cat_c27)) {
 #   color <- cat_all$color[i]
 #   cat_all$off[i] <- -all_off
 #   rect(0, -all_off, 2, -(cat_all$percent[i] + all_off), col = color, border = color, xpd = T)
