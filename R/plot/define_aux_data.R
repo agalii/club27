bbcbab8f7f957ccc5f8229950998564178eca831
agalii <git@lisa-angermann.de>
2017-02-05 00:08:43 +0100

    new colors!

--- R/plot/define_aux_data.R
+++ R/plot/define_aux_data.R
@@ -1,25 +1,35 @@
+############################################################
+# define color ramp and legend names for all groups
+# cmin <- c(23, 190, 207)  # dark turquois 
+# clow <- c(158, 218, 229) # light turquois
+cmin <- c(188, 189, 34)  # dark green
+clow <- c(219, 219, 141) # light green
+chig <- c(174, 199, 232) # light blue
+cmax <- c(31, 119, 180)  # dark blue
+ColorMax <- rgb(214, 39, 40, maxColorValue = 255)   # dark red
+# dark blue - light blue
+ColorMHi <- rgb(seq(cmax[1], chig[1], length = 4),  # Red
+                seq(cmax[2], chig[2], length = 4),  # Green
+                seq(cmax[3], chig[3], length = 4),  # Blue
+                maxColorValue = 255)
+# light blue - light green
+ColorMLo <- rgb(seq(chig[1], clow[1], length = 7), 
+                seq(chig[2], clow[2], length = 7),
+                seq(chig[3], clow[3], length = 7), 
+                maxColorValue = 255)
+# light green - dark green 
+ColorLow <- rgb(seq(clow[1], cmin[1], length = 4),
+                seq(clow[2], cmin[2], length = 4),
+                seq(clow[3], cmin[3], length = 4), 
+                maxColorValue = 255)
 
-# prelim colors
-tableau20 = c(rgb(31, 119, 180, maxColorValue = 255), rgb(174, 199, 232, maxColorValue = 255), 
-              rgb(255, 127, 14, maxColorValue = 255), rgb(255, 187, 120, maxColorValue = 255),  
-              rgb(44, 160, 44, maxColorValue = 255), rgb(152, 223, 138, maxColorValue = 255), 
-              rgb(214, 39, 40, maxColorValue = 255), rgb(255, 152, 150, maxColorValue = 255),  
-              rgb(148, 103, 189, maxColorValue = 255), rgb(197, 176, 213, maxColorValue = 255), 
-              rgb(140, 86, 75, maxColorValue = 255), rgb(196, 156, 148, maxColorValue = 255),  
-              rgb(227, 119, 194, maxColorValue = 255), rgb(247, 182, 210, maxColorValue = 255), 
-              rgb(127, 127, 127, maxColorValue = 255), rgb(199, 199, 199, maxColorValue = 255),  
-              rgb(188, 189, 34, maxColorValue = 255), rgb(219, 219, 141, maxColorValue = 255), 
-              rgb(23, 190, 207, maxColorValue = 255), rgb(158, 218, 229, maxColorValue = 255))  
-
-# plot(1:20, c(1:10, 1:10), pch = 19, col = tableau20, cex = 5)
-
-i_col = c(4, 14, 8, 1, 20, 19, 10, 13, 18, 7, 12, 17, 15, 2)
-aux_data <- data.frame(cat    = c('showbiz', 'actor', 'musician', 'singer', 'artist', 'athlete', 'author', 
-                                  'journalist', 'military', 'resistance', 'cleric', 'scientist', 'politician', 'other'),
-                       legend = c('Showbiz', 'Schauspieler:in', 'Musiker:in', 'Sänger:in', 'Künstler:in', 
-                                  'Athlet:in', 'Autor:in', 'Journalist:in', 'Militär', 'Widerstandskämpfer:in', 
-                                  'Geistliche:r', 'Wissenschaftler:in', 'Politiker:in', 'andere'), 
-                       color  = tableau20[i_col],
+aux_data <- data.frame(cat    = c('resistance', 'athlete', 'singer', 'other', 'military', 'author', 'actor', 
+                                  'musician', 'artist', 'journalist', 'cleric', 'showbiz', 'scientist', 'politician'),
+                       legend = c('Widerstandskämpfer:in', 'Athlet:in', 'Sänger:in', 'andere', 'Militär', 'Autor:in', 'Schauspieler:in', 
+                                  'Musiker:in', 'Künstler:in', 'Journalist:in', 'Geistliche:r', 'Showbiz', 'Wissenschaftler:in', 'Politiker:in'), 
+                       # color  = c(ColorMax, rev(c(ColorHigh, ColorMiHi[-1], ColorMiLo[-1], ColorLow[-1]))),
+                       color  = c(ColorMax, ColorMHi, ColorMLo[-1], ColorLow[-1]),
                        stringsAsFactors = F)
+
 
 write.csv(aux_data, file = 'data/output/data_to_plot/aux_data_for_plots.csv', row.names = F)
